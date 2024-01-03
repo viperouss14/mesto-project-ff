cd486cb960f0c7d4a45629fe3b4c72f7f3e5ff7e
Victor Nepomnyashchiy <viperouss@yandex.ru>
2024-01-03 15:03:48 +0300

    Первое ревью

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Проектная работа Mesto
+# Проектная работа Mesto <br>
+_________________________________
 
+[Ссылка на репозиторий](https://github.com/viperouss14/mesto-project-ff.git) <br>
+
+[Ссылка на макет Figma](https://www.figma.com/file/bjyvbKKJN2naO0ucURl2Z0/JavaScript.-Sprint-5?node-id=0%3A1&mode=dev) <br>
+
+[Ссылка на Практикум](https://practicum.yandex.ru/trainer/frontend-developer/lesson/b982cec5-e2c8-4b4a-957e-65e4b0e42bc1/) <br>
+
+________________________________
+
+## Новые скилы работы с JavaScript:
+- методы работы с данными, условия, циклы
+- массивы
+- функции
+- дебаггинг JavaScript
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template');
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(cardData, cardDeleteFunc) {
+  const cardElement = cardTemplate.content.cloneNode(true);
+
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  cardDeleteButton.addEventListener('click', cardDeleteFunc)
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
-
+function deleteCard(event) {
+  event.target.closest('.card').remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(cardData) {
+  const cardElement = createCard(cardData, deleteCard);
+  placesList.append(cardElement);
+});
